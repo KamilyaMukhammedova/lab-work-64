6e4463bc36273847207ace85f345ed57b8e42bfc
Kamilya Mukhammedova <kamilichkam@gmail.com>
2022-07-08 21:48:49 +0600

    #2 Реализация создания и отправки нового поста

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import './Navigation.css';
+
+const Navigation = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <button type="button" className="btn btn-outline-light mr-4 mb-2">
+          <NavLink to="/" className="navLink">My blog</NavLink>
+        </button>
+        <div>
+          <ul className="nav">
+            <li className="nav-item">
+              <button type="button" className="btn btn-outline-light mr-3 mb-2">
+                <NavLink to="/" className="navLink">Home</NavLink>
+              </button>
+            </li>
+            <li className="nav-item">
+              <button type="button" className="btn btn-outline-light mr-3 mb-2">
+                <NavLink to="/posts/add" className="navLink">Add</NavLink>
+              </button>
+            </li>
+            <li className="nav-item">
+              <button type="button" className="btn btn-outline-light mr-3 mb-2">
+                <NavLink to="/about" className="navLink">About</NavLink>
+              </button>
+            </li>
+            <li className="nav-item">
+              <button type="button" className="btn btn-outline-light mr-3 mb-2">
+                <NavLink to="/contacts" className="navLink">Contacts</NavLink>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
---
+++ src/containers/Form/Form.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import axiosApi from "../../axiosApi";
+import dayjs from "dayjs";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { convertToHTML } from 'draft-convert';
+
+const Form = ({history}) => {
+  const [newPost, setNewPost] = useState({
+    title: '',
+    description: '',
+    date: dayjs().format('DD.MM.YYYY HH:mm:ss'),
+  });
+
+  const onEditorStateChange = (editor) => {
+    setNewPost(prev => ({
+      ...prev,
+      description: convertToHTML(editor.getCurrentContent())
+    }))
+  };
+
+
+  const onTitleChange = (e) => {
+    const {name, value} = e.target;
+    setNewPost(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const addNewPost = async e => {
+    e.preventDefault();
+
+    try {
+      await axiosApi.post('/posts.json', {newPost});
+    } finally {
+      history.push('/');
+    }
+  };
+
+  return (
+    <div className="row justify-content-center mt-5">
+      <form className="col-5 border p-3" onSubmit={addNewPost}>
+        <h2>Add new post</h2>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            type="text"
+            className="form-control"
+            name="title"
+            value={newPost.title}
+            onChange={onTitleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Description</label>
+          <Editor
+            defaultEditorState={newPost.description}
+            toolbarClassName="toolbarClassName"
+            wrapperClassName="wrapperClassName"
+            editorClassName="editorClassName"
+            onEditorStateChange={onEditorStateChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-success">Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
