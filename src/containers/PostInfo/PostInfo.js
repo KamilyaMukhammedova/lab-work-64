f12c5c9b3a033acec469863497fc39fe460137d1
Kamilya Mukhammedova <kamilichkam@gmail.com>
2022-07-09 00:04:51 +0600

    #5 Реализация удаления одного поста

--- src/containers/PostInfo/PostInfo.js
+++ src/containers/PostInfo/PostInfo.js
@@ -2,17 +2,29 @@ import React, {useEffect, useState} from 'react';
 import axiosApi from "../../axiosApi";
 import parse from 'html-react-parser';
 
-const PostInfo = ({match}) => {
+const PostInfo = ({match, history}) => {
   const [post, setPost] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
-      const response = await axiosApi(`/posts/${match.params.id}.json`);
-      setPost(response.data);
+      try {
+        const response = await axiosApi(`/posts/${match.params.id}.json`);
+        setPost(response.data);
+      } catch (e) {
+        console.error(e);
+      }
     };
 
     fetchPost().catch(e => console.error(e));
   }, [match.params.id]);
+
+  const onRemove = async () => {
+    try {
+      await axiosApi.delete(`/posts/${match.params.id}.json`);
+    } finally {
+      history.push('/');
+    }
+  };
 
   return post && (
     <div className="alert alert-success m-5 col-6">
@@ -21,7 +33,13 @@ const PostInfo = ({match}) => {
       <>
         {parse(post.newPost.description)}
       </>
-      <button type="button" className="btn btn-danger mr-3">Remove</button>
+      <button
+        type="button"
+        className="btn btn-danger mr-3"
+        onClick={onRemove}
+      >
+        Remove
+      </button>
       <button type="button" className="btn btn-dark">Edit</button>
     </div>
   );
